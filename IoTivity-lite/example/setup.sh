38b86ab18d482e5c3830001347c23e7c774f9ffb
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-08-31 23:13:50 -0600

    Changes to setup.sh script

--- IoTivity-lite/example/setup.sh
+++ IoTivity-lite/example/setup.sh
@@ -2,7 +2,7 @@
 CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
-MY_COMMAND="cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/example/Makefile $CURPWD/src/"
+MY_COMMAND="cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/example/Makefile $CURPWD/"
 eval ${MY_COMMAND}
 MY_COMMAND="cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/pi-boards/example/example.json $CURPWD/$PROJNAME.json"
 eval ${MY_COMMAND}
