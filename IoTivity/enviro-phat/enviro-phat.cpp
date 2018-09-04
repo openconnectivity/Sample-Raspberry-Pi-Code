9b2d437abb7138a5625b582f93d977dd394aaeb3
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-09-04 16:25:22 -0600

    counter fix

--- IoTivity/enviro-phat/enviro-phat.cpp
+++ IoTivity/enviro-phat/enviro-phat.cpp
@@ -390,7 +390,8 @@ void BrightnessResource::brightnessObserverLoop()
     //    testEnviroPhat->CallPythonFunction((char *)"enviro-phat", (char *)"readADC", 1, testEnviroPhat->myParamArgs);
     //    m_var_value_voltage = testEnviroPhat->returnDouble;
 
-    counter = ++counter % 10;
+    counter++;
+    counter = counter % 10;
     m_var_value_brightness = counter;
 
     std::cout << "\t\t" << "property 'brightness' : "<< m_var_value_brightness << std::endl;
