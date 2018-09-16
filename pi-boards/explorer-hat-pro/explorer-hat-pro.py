1afba00426bff19307d50e6b41398a45ccc25f3c
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-09-16 10:13:27 -0600

    added event handlers in Python interface code

--- pi-boards/explorer-hat-pro/explorer-hat-pro.py
+++ pi-boards/explorer-hat-pro/explorer-hat-pro.py
@@ -11,6 +11,16 @@ def readAnalog(channel):
     print "voltage value = ", voltage
     return float(voltage)
 
+def handleAnalogInput(input, voltage):
+    channel = input.name()
+    print("Analog input handler channel {} changed to {} volts.".format(channel, voltage))
+
+def startObserveAnalogInput(channel):
+    explorerhat.analog[channel - 1].changed(handleAnalogInput, 0.1)
+
+def stopObserveAnalogInput(channel):
+    explorerhat.analog[channel - 1].clear_events()
+
 def readInput(channel):
     if 0 < channel < 5:
         state = explorerhat.input[channel - 1].read()
@@ -20,6 +30,17 @@ def readInput(channel):
     print "Input value = ", state
     return int(state)
 
+def handleInput(input):
+    state = input.read()
+    channel = input.name()
+    print("Input handler channel {} changed to {}.".format(channel, state))
+
+def startObserveInput(channel):
+    explorerhat.input[channel - 1].changed(handleInput, 25)
+
+def stopObserveInput(channel):
+    explorerhat.input[channel - 1].clear_events()
+
 def readTouch(channel):
     if 0 < channel < 9:
         state = explorerhat.touch[channel - 1].read()
@@ -28,6 +49,20 @@ def readTouch(channel):
         print "Specified touch channel is out of range."
     print "touch value = ", state
     return int(state)
+
+def handleTouch(channel, event):
+    if event == 'press':
+        print "press"
+    if event == 'release'
+        print "release"
+    print("Touch handler channel {} changed to {}.".format(channel, event))
+
+def startObserveTouch(channel):
+    explorerhat.touch[channel - 1].pressed(handleTouch)
+    explorerhat.touch[channel - 1].released(handleTouch)
+
+def stopObserveTouch(channel):
+    explorerhat.touch[channel - 1].clear_events()
 
 def writeOutput(channel, state):
     if 0 < channel < 5:
