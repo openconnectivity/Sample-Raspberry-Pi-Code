c70fde24355d059daf705da1bc8b03a8b8ede9eb
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-04 13:51:57 -0600

    fixing script error

--- IoTivity/explorer-hat-pro/setup.sh
+++ IoTivity/explorer-hat-pro/setup.sh
@@ -8,5 +8,4 @@ cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/explorer-ha
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/observer.cpp ${CURPWD}/src/
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/observer.h ${CURPWD}/src/
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/namedefs.h ${CURPWD}/src/
-mv -i $CURPWD/src/explorer-hat-pro.cpp ${CURPWD}/src/${PROJNAME}.cpp
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/pi-boards/explorer-hat-pro/explorer-hat-pro.py ${CURPWD}/bin/
