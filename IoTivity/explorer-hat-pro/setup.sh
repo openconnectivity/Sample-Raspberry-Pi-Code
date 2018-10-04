b7c352ee5f05ba111c5aa517f0312d20815a5e00
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-04 13:46:25 -0600

    Fixing some script buts

--- IoTivity/explorer-hat-pro/setup.sh
+++ IoTivity/explorer-hat-pro/setup.sh
@@ -5,7 +5,8 @@ PROJNAME=${PWD##*/}
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/SConscript ${CURPWD}/
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/pi-boards/explorer-hat-pro/explorer-hat-pro.json ${CURPWD}/${PROJNAME}.json
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/explorer-hat-pro-gen.cpp ${CURPWD}/src/${PROJNAME}-gen.cpp
-cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/*.cpp ${CURPWD}/src/
-cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/*.h ${CURPWD}/src/
+cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/observer.cpp ${CURPWD}/src/
+cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/observer.h ${CURPWD}/src/
+cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/namedefs.h ${CURPWD}/src/
 mv -i $CURPWD/src/explorer-hat-pro.cpp ${CURPWD}/src/${PROJNAME}.cpp
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/pi-boards/explorer-hat-pro/explorer-hat-pro.py ${CURPWD}/bin/
