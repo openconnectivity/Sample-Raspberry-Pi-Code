762cc37c029bf1738416ad4a408334fe11d5e177
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-04 14:37:12 -0600

    missing instruction to copy file

--- IoTivity/explorer-hat-pro/setup.sh
+++ IoTivity/explorer-hat-pro/setup.sh
@@ -4,6 +4,7 @@ PROJNAME=${PWD##*/}
 
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/SConscript ${CURPWD}/
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/pi-boards/explorer-hat-pro/explorer-hat-pro.json ${CURPWD}/${PROJNAME}.json
+cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/explorer-hat-pro.cpp ${CURPWD}/src/${PROJNAME}.cpp
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/explorer-hat-pro-gen.cpp ${CURPWD}/src/${PROJNAME}-gen.cpp
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/observer.cpp ${CURPWD}/src/
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/explorer-hat-pro/observer.h ${CURPWD}/src/
