c6f7aa532fa4c4b86bc4588128a5b5d786abb38d
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-14 15:45:19 -0600

    config file

--- IoTivity/automation-phat/setup.sh
+++ IoTivity/automation-phat/setup.sh
@@ -3,6 +3,7 @@ CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/automation-phat/SConscript ${CURPWD}/
+cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/automation-phat/automation-phat-config.json ${CURPWD}/${PROJNAME}-config.json
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/pi-boards/automation-phat/automation-phat.json ${CURPWD}/${PROJNAME}.json
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/automation-phat/automation-phat.cpp ${CURPWD}/src/${PROJNAME}.cpp
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/automation-phat/automation-phat-gen.cpp ${CURPWD}/src/${PROJNAME}-gen.cpp
