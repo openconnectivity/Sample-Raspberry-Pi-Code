3581f77c2b9efdd1bdd03a968d3abd8ac0a707b9
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-14 15:57:26 -0600

    config file

--- IoTivity-lite/mraa-example/setup.sh
+++ IoTivity-lite/mraa-example/setup.sh
@@ -3,6 +3,7 @@ CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/mraa-example/Makefile ${CURPWD}/
+cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/mraa-example/mraa-example-config.json ${CURPWD}/${PROJNAME}-config.json
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/pi-boards/mraa-example/mraa-example.json ${CURPWD}/${PROJNAME}.json
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/mraa-example/mraa-example.c ${CURPWD}/src/${PROJNAME}.c
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/mraa-example/mraa-example-gen.c ${CURPWD}/src/${PROJNAME}-gen.c
