316be43dfdc72a7555143e06433574e6037968d4
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-14 15:25:56 -0600

    config file

--- IoTivity/enviro-phat/setup.sh
+++ IoTivity/enviro-phat/setup.sh
@@ -3,6 +3,7 @@ CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/enviro-phat/SConscript ${CURPWD}/
+cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/enviro-phat/enviro-phat-config.json ${CURPWD}/${PROJNAME}-config.json
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/pi-boards/enviro-phat/enviro-phat.json ${CURPWD}/${PROJNAME}.json
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/enviro-phat/enviro-phat-gen.cpp ${CURPWD}/src/${PROJNAME}-gen.cpp
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/enviro-phat/*.cpp ${CURPWD}/src/
