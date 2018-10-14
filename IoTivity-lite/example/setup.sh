57eb0744575bcad45b67354d86a288669bc7cdcb
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-14 01:04:43 -0600

    fixes

--- IoTivity-lite/example/setup.sh
+++ IoTivity-lite/example/setup.sh
@@ -2,6 +2,6 @@
 CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
-cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/example/Makefile ${CURPWD}/
-cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/example/example-config.json ${CURPWD}/${PROJNAME}-config.json
+cp -f ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/example/Makefile ${CURPWD}/Makefile
+cp -f ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/example/example-config.json ${CURPWD}/${PROJNAME}-config.json
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/pi-boards/example/example.json ${CURPWD}/${PROJNAME}.json
