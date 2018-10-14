d295abca586f1a5f5e662a58a1772b9dafe704c0
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-14 15:15:15 -0600

    various fixes

--- IoTivity/mraa-example/setup.sh
+++ IoTivity/mraa-example/setup.sh
@@ -3,6 +3,7 @@ CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/mraa-example/SConscript ${CURPWD}/
+cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/mraa-example/mraa-example-config.json ${CURPWD}-config.json
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/pi-boards/mraa-example/mraa-example.json ${CURPWD}/${PROJNAME}.json
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/mraa-example/mraa-example.cpp ${CURPWD}/src/${PROJNAME}.cpp
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity/mraa-example/mraa-example-gen.cpp ${CURPWD}/src/${PROJNAME}-gen.cpp
