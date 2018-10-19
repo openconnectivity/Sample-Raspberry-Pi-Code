cea2fc6bd1890994b21b9dd30382fc9d92b4769d
Clarke Stevens <clarke.stevens@sjrb.ca>
2018-10-19 09:55:05 -0600

    reinstalling observation stuff

--- IoTivity/tutorial/tutorial.cpp
+++ IoTivity/tutorial/tutorial.cpp
@@ -288,7 +288,7 @@ BinaryswitchResource::BinaryswitchResource(std::string resourceUri)
     // initialize vector rt  Resource Type
     m_var_value_rt.push_back("oic.r.switch.binary");
     m_var_value_value = false; // current value of property "value" Status of the switch
-    }
+}
 
 /*
 * Destructor code
@@ -774,8 +774,8 @@ class DimmingswitchResource : public Resource
         const int MAX_BRIGHTNESS=65535;
 
         //observer callback functions
-//        shared_ptr<IoTObserver> m_dimmingswitchObserverLoop;
-//        void dimmingswitchObserverLoop();
+        shared_ptr<IoTObserver> m_dimmingswitchObserverLoop;
+        void dimmingswitchObserverLoop();
 
     private:
 
@@ -852,8 +852,8 @@ DimmingswitchResource::DimmingswitchResource(std::string resourceUri)
     m_var_value_rt.push_back("oic.r.light.dimming");
 
     // set up the observation dimmingswitchObserverLoop
-//    IoTObserverCb dimmingswitchObsCb = bind(&DimmingswitchResource::dimmingswitchObserverLoop, this);
-//    m_dimmingswitchObserverLoop = make_shared<IoTObserver>(dimmingswitchObsCb);
+    IoTObserverCb dimmingswitchObsCb = bind(&DimmingswitchResource::dimmingswitchObserverLoop, this);
+    m_dimmingswitchObserverLoop = make_shared<IoTObserver>(dimmingswitchObsCb);
 }
 
 /*
@@ -942,7 +942,6 @@ OCStackResult DimmingswitchResource::sendNotification(const std::shared_ptr< OCR
 /*
 * Observer loop for the  observe function /Dimmingswitch
 */
-/*
 void DimmingswitchResource::dimmingswitchObserverLoop()
 {
     usleep(1500000);
@@ -955,7 +954,7 @@ void DimmingswitchResource::dimmingswitchObserverLoop()
     std::cout << "\t\t" << "property 'dimmingSetting' : "<< m_var_value_dimmingSetting << std::endl;
     m_rep.setValue(m_var_name_dimmingSetting, m_var_value_dimmingSetting );
 }
-*/
+
 
 /*
 * Make the payload for the retrieve function (e.g. GET) /dimmingswitch
@@ -1298,12 +1297,12 @@ OCEntityHandlerResult DimmingswitchResource::entityHandler(std::shared_ptr<OCRes
                 // add observer
                 std::cout << "Starting observer for dimmingswitch sensor" << std::endl;
                 m_interestedObservers.push_back(observationInfo.obsId);
-//                m_dimmingswitchObserverLoop->start();
+                m_dimmingswitchObserverLoop->start();
             }
             else if(ObserveAction::ObserveUnregister == observationInfo.action)
             {
                 std::cout << "Stopping observer for dimmingswitch sensor" << std::endl;
-//                m_dimmingswitchObserverLoop->stop();
+                m_dimmingswitchObserverLoop->stop();
 
                 // delete observer
                 m_interestedObservers.erase(std::remove(
