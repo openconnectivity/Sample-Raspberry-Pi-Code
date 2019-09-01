f330aa9de62630d68e504ef173b222457fa61b3b
Clarke Stevens <clarke.stevens@sjrb.ca>
2019-09-01 10:08:52 -0600

    updating explorer hat files

--- IoTivity-lite/explorer-hat-pro/setup.sh
+++ IoTivity-lite/explorer-hat-pro/setup.sh
@@ -2,7 +2,7 @@
 CURPWD=`pwd`
 PROJNAME=${PWD##*/}
 
-cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/explorer-hat-pro/Makefile ${CURPWD}/Makefile
+cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/explorer-hat-pro/devbuildmake ${CURPWD}/devbuildmake
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/explorer-hat-pro/explorer-hat-pro-config.json ${CURPWD}/${PROJNAME}-config.json
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/pi-boards/explorer-hat-pro/explorer-hat-pro.json ${CURPWD}/${PROJNAME}.json
 cp ${EXAMPLEPATH}/Sample-Raspberry-Pi-Code/IoTivity-lite/explorer-hat-pro/explorer-hat-pro.c ${CURPWD}/src/${PROJNAME}.c
